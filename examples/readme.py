be04f52c36d5c93714cb04219a0ee59816d5ad88
Arijit Basu <sayanarijit@gmail.com>
2019-06-23 00:03:05 +0530

    Readme example rewrite

--- examples/readme.py
+++ examples/readme.py
@@ -1,64 +1,69 @@
 from htmldoom import elements as e
+from htmldoom.layouts import Component
 
-doc = e.Div()(
-    e.H1(align="center")("htmldoom"),
-    e.P(align="center")("Write safer and cleaner HTML using Python"),
-    e.P(align="center")(
-        e.Span()(
-            e.A(href="https://pypi.org/project/htmldoom")(
-                e.Img(
-                    src="https://img.shields.io/pypi/v/htmldoom.svg", alt="PyPI version"
-                )
-            )
-        ),
-        b"&nbsp;",
-        e.Span()(
-            e.A(href="https://pypi.org/project/htmldoom")(
-                e.Img(
-                    src="https://img.shields.io/pypi/pyversions/htmldoom.svg",
-                    alt="PyPI pyversions",
-                )
-            )
-        ),
-        b"&nbsp;",
-        e.Span()(
-            e.A(href="https://travis-ci.org/sayanarijit/htmldoom")(
-                e.Img(
-                    src="https://travis-ci.org/sayanarijit/htmldoom.svg?branch=master",
-                    alt="Build Status",
-                )
-            )
-        ),
-        b"&nbsp;",
-        e.Span()(
-            e.A(href="https://codecov.io/gh/sayanarijit/htmldoom")(
-                e.Img(
-                    src="https://codecov.io/gh/sayanarijit/htmldoom/branch/master/graph/badge.svg",
-                    alt="codecov",
-                )
-            )
-        ),
-        b"&nbsp;",
-        e.Span()(
-            e.A(href="https://github.com/python/black")(
-                e.Img(
-                    src="https://img.shields.io/badge/code%20style-black-000000.svg",
-                    alt="Code style: black",
-                )
-            )
-        ),
-    ),
-    e.P()(
-        e.H2()("Usage"),
-        e.P()(
+
+class ReadMe(Component):
+    """The README.md generator"""
+
+    @property
+    def header(self) -> e.H1:
+        return e.H1(align="center")("htmldoom")
+
+    @property
+    def description(self) -> e.P:
+        return e.P(align="center")("Write safer and cleaner HTML using Python")
+
+    def badge(self, href: str, src: str, alt: str) -> e.Span:
+        return e.Span()(e.A(href=href)(e.Img(src=src, alt=alt)))
+
+    @property
+    def badges(self) -> e.P:
+        return e.P(align="center")(
+            self.badge(
+                href="https://pypi.org/project/htmldoom",
+                src="https://img.shields.io/pypi/v/htmldoom.svg",
+                alt="PyPI version",
+            ),
+            b"&nbsp;",
+            self.badge(
+                href="https://pypi.org/project/htmldoom",
+                src="https://img.shields.io/pypi/pyversions/htmldoom.svg",
+                alt="PyPI version",
+            ),
+            b"&nbsp;",
+            self.badge(
+                href="https://travis-ci.org/sayanarijit/htmldoom",
+                src="https://travis-ci.org/sayanarijit/htmldoom.svg?branch=master",
+                alt="Build Status",
+            ),
+            b"&nbsp;",
+            self.badge(
+                href="https://codecov.io/gh/sayanarijit/htmldoom",
+                src="https://codecov.io/gh/sayanarijit/htmldoom/branch/master/graph/badge.svg",
+                alt="codecov",
+            ),
+            b"&nbsp;",
+            self.badge(
+                href="https://github.com/python/black",
+                src="https://img.shields.io/badge/code%20style-black-000000.svg",
+                alt="Code style: black",
+            ),
+        )
+
+    @property
+    def example1(self) -> e.P:
+        return e.P()(
             e.Pre()(
                 ">>> from htmldoom import elements as e\n"
                 ">>> \n"
                 """>>> e.P(style=e.style(color="red"))("This is a paragraph")\n"""
                 """<p style="color:'red';">This is a paragraph</p>"""
             )
-        ),
-        e.P()(
+        )
+
+    @property
+    def example2(self) -> e.P:
+        return e.P()(
             e.Pre()(
                 ">>> from htmldoom import elements as e\n"
                 ">>> from htmldoom.layouts import BaseLayout\n"
@@ -75,57 +80,85 @@ doc = e.Div()(
                 "<!DOCTYPE html>\n"
                 "<html><head><title>foo</title></head><body>Welcome bar</body></html>"
             )
-        ),
-        e.P()(
+        )
+
+    @property
+    def more_examples(self) -> e.P:
+        return e.P()(
             e.A(href="https://github.com/sayanarijit/htmldoom/tree/master/examples")(
                 "Find more examples here"
             )
-        ),
-    ),
-    e.P()(
-        e.H2()("Benchmarks"),
-        e.P()(
-            "Very basic benchmark done using ",
-            e.A(
-                href="https://github.com/sayanarijit/htmldoom/blob/master/benchmark/general.py"
-            )("this script"),
-            " and IPython",
-        ),
-        e.H3()("htmldoom"),
-        e.Img(
-            src="https://thepracticaldev.s3.amazonaws.com/i/6dmd4r7lgoqu9wrv4wr9.png",
-            alt="htmldoom stats",
-        ),
-        e.H3()("Jinja2"),
-        e.Img(
-            src="https://thepracticaldev.s3.amazonaws.com/i/hvvuvybfk5jved6trinr.png",
-            alt="Jinja2 stats",
-        ),
-        e.H3()("Mako"),
-        e.Img(
-            src="https://thepracticaldev.s3.amazonaws.com/i/xyzdag8221qzoohz1tx9.png",
-            alt="Mako stats",
-        ),
-        e.H3()("Chameleon"),
-        e.Img(
-            src="https://thepracticaldev.s3.amazonaws.com/i/0j49ln7pa62ivhqzkkuq.png",
-            alt="Chameleon stats",
-        ),
-        e.H3()("Conclusion"),
-        e.P()(
-            "htmldoom performs best upto a certain number of loops which is generally high enough."
-            " These measurements are very naive and shows very basic information."
-        ),
-    ),
-    e.P()(
-        e.H2()("Contributing"),
-        e.P()(
-            "Check out the ",
-            e.A(
-                href="https://github.com/sayanarijit/htmldoom/tree/master/CONTRIBUTING.md"
-            )(" contributing guidelines."),
-        ),
-    ),
-)
+        )
 
-print(doc)
+    @property
+    def usage(self) -> e.P:
+        return e.P()(e.H2()("Usage"), self.example1, self.example2, self.more_examples)
+
+    def benchmark(self, title: str, src: str, alt: str) -> e.P:
+        return e.P()(e.H3()(title), e.Img(src=src, alt=alt))
+
+    @property
+    def benchmarks(self) -> e.P:
+        return e.P()(
+            e.H2()("Benchmarks"),
+            e.P()(
+                "Very basic benchmark done using ",
+                e.A(
+                    href="https://github.com/sayanarijit/htmldoom/blob/master/benchmark/general.py"
+                )("this script"),
+                " and IPython",
+            ),
+            self.benchmark(
+                title="htmldoom",
+                src="https://thepracticaldev.s3.amazonaws.com/i/6dmd4r7lgoqu9wrv4wr9.png",
+                alt="htmldoom stats",
+            ),
+            self.benchmark(
+                title="Jinja2",
+                src="https://thepracticaldev.s3.amazonaws.com/i/hvvuvybfk5jved6trinr.png",
+                alt="Jinja2 stats",
+            ),
+            self.benchmark(
+                title="Mako",
+                src="https://thepracticaldev.s3.amazonaws.com/i/xyzdag8221qzoohz1tx9.png",
+                alt="Mako stats",
+            ),
+            self.benchmark(
+                title="Chameleon",
+                src="https://thepracticaldev.s3.amazonaws.com/i/0j49ln7pa62ivhqzkkuq.png",
+                alt="Chameleon stats",
+            ),
+            e.H3()("Conclusion"),
+            e.P()(
+                "htmldoom performs best upto a certain number of loops which is generally high enough."
+                " These measurements are very naive and shows very basic information."
+            ),
+        )
+
+    @property
+    def contributing(self) -> e.P:
+        return e.P()(
+            e.H2()("Contributing"),
+            e.P()(
+                "Check out the ",
+                e.A(
+                    href="https://github.com/sayanarijit/htmldoom/tree/master/CONTRIBUTING.md"
+                )(" contributing guidelines."),
+            ),
+        )
+
+    @property
+    def render(self) -> e.Div:
+        return e.Div()(
+            self.header,
+            self.description,
+            self.badges,
+            self.usage,
+            self.benchmarks,
+            self.contributing,
+        )
+
+
+if __name__ == "__main__":
+    readme = ReadMe()
+    print(readme)
