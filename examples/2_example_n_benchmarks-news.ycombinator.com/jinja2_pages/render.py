f0b449615069d6544e8cd61b0dd7942626716feb
Arijit Basu <sayanarijit@gmail.com>
2019-07-01 23:39:36 +0530

    Add example with dynamic data with benchmarks

---
+++ examples/2_example_n_benchmarks-news.ycombinator.com/jinja2_pages/render.py
@@ -0,0 +1,19 @@
+import json
+
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+
+env = Environment()
+env.loader = FileSystemLoader(".")
+jinja2_renderer = env.get_template("jinja2_pages/index.html")
+
+with open("news.json") as f:
+    news = json.load(f)
+
+
+def render(count: int = 500):
+    return jinja2_renderer.render({"news": {i: news for i in range(count)}})
+
+
+if __name__ == "__main__":
+    print(render())
