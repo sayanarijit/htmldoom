8b6d96bd83f6720ef913e064e8863da7bbf55ec5
Arijit Basu <sayanarijit@gmail.com>
2019-09-03 13:51:29 +0530

    Fix docstrings

--- htmldoom/__init__.py
+++ htmldoom/__init__.py
@@ -3,7 +3,7 @@ __email__ = "sayanarijit@gmail.com"
 __homepage__ = "https://github.com/sayanarijit/htmldoom"
 __description__ = "An intuitive, high performance HTML rendering framework"
 
-__version__ = "v0.6.8"
+__version__ = "v0.6.9"
 __license__ = "MIT"
 __all__ = [
     "__author__",
--- htmldoom/url.py
+++ htmldoom/url.py
@@ -2,9 +2,11 @@
 
 from urllib.parse import urlencode, urljoin
 
+__all__ = ["url", "http", "https"]
+
 
 def url(scheme, domain, *paths, **params):
-    """Generate safe https url.
+    """Generate safe URL.
     
     Example:
         >>> print(url("https", "foo.com", "bar", page=1, sortby=["id", "date"]))
@@ -21,21 +23,21 @@ def url(scheme, domain, *paths, **params):
     )
 
 
-def https(domain, *paths, **params):
-    """Generate safe https url.
-    
-    Example:
-        >>> print(https("foo.com", "bar", page=1, sortby=["id", "date"]))
-        https://foo.com/bar?page=1&sortby=id&sortby=date
-    """
-    return url("https", domain, *paths, **params)
-
-
 def http(domain, *paths, **params):
-    """Generate safe https url.
+    """Generate safe http URL.
     
     Example:
         >>> print(http("foo.com", "bar", page=1, sortby=["id", "date"]))
         http://foo.com/bar?page=1&sortby=id&sortby=date
     """
     return url("http", domain, *paths, **params)
+
+
+def https(domain, *paths, **params):
+    """Generate safe https URL.
+    
+    Example:
+        >>> print(https("foo.com", "bar", page=1, sortby=["id", "date"]))
+        https://foo.com/bar?page=1&sortby=id&sortby=date
+    """
+    return url("https", domain, *paths, **params)
