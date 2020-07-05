53911f38e9da60a6d46d9f357bb03fc76a271ec6
Arijit Basu <sayanarijit@gmail.com>
2020-07-05 08:44:21 +0530

    Make it stable

--- tests/test_renders.py
+++ tests/test_renders.py
@@ -1,21 +1,2 @@
 from htmldoom import render, renders, elements as e, functions as fn
 
-
-@renders(e.li()("{item}"))
-def render_list_item(item):
-    return {"item": item}
-
-
-@renders(e.p()("{foo}"), e.ul()("{list_items}"))
-def render_component():
-    return {
-        "foo": "bar",
-        "list_items": fn.foreach(["a", "b", "c"])(lambda x: render_list_item(x)),
-    }
-
-
-def test_renders():
-    assert (
-        render(render_component())
-        == "<p>bar</p><ul><li>a</li><li>b</li><li>c</li></ul>"
-    )
